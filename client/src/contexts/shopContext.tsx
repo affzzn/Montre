6bbcbfd745af8decd99ddd94b46a7288e6e56093
Affan Imran <affanimran@Affans-Air.lan>
2024-04-02 06:12:20 +0400

    context prog

--- client/src/contexts/shopContext.tsx
+++ client/src/contexts/shopContext.tsx
@@ -1,12 +1,28 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
-interface IShopContext {}
+interface IShopContext {
+  addToCart: (itemId: string) => void;
+  removeFromCart: (itemId: string) => void;
+  updateItemCount: (newAmount: number, itemId: string) => void;
+}
 
-const defaultVal: IShopContext = {};
+const defaultVal: IShopContext = {
+  addToCart: () => null,
+  removeFromCart: () => null,
+  updateItemCount: () => null,
+};
 
 export const ShopContext = createContext<IShopContext>(defaultVal);
 
 export const ShopContextProvider = ({ children }) => {
+  const [cartItems, setCartItems] = useState<{ string: number } | {}>({});
+
+  const addToCart = (itemId: string) => {};
+
+  const removeFromCart = (itemId: string) => {};
+
+  const updateItemCount = (newAmount: number, itemId: string) => {};
+
   const contextVal: IShopContext = {};
   return (
     <ShopContext.Provider value={contextVal}>{children}</ShopContext.Provider>
